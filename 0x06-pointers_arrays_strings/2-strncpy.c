8999663fee1a85404a5c9e3454443824bab36ac7
Abdelmoneim00 <ab000arabi22@gmail.com>
2023-03-29 00:39:16 -0700

    create a function to copy src string and paste it into dest string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy a string into another array of string
+ * @dest: array of string to be modified
+ * @src: array of string to be copied
+ * Return: dest array after modification
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i <n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
