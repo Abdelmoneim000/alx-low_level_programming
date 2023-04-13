fcc1a6f88fe9dd2fb1646173c4a6c9dcb63690ff
Abdel-Moneim Arabi <ab000arabi22@gmail.com>
2023-04-14 00:44:46 +0200

    create a function that concatenates two strings

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,7 @@ void init(char *str, int l);
 #include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 
 #endif/*MAIN_H*/
