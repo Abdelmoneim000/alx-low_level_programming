ee43a8b712ae8b97a27ca632686ca61a76a80152
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-07-09 05:14:40 +0300

    fix the allocated memory for task 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,10 +2,18 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new = malloc(sizeof(dlistint_t));
+    dlistint_t *new = malloc(sizeof(dlistint_t) + 4);
+    if (new == NULL)
+    {
+        return (NULL);
+    }
     new->next = *head;
     new->prev = NULL;
     new->n = n;
+    if (*head != NULL)
+    {
+        (*head)->prev = new;
+    }
     *head = new;
 
     return (*head);
