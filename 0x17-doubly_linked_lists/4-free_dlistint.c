71c3eac624eda357cdf3d6f911824bfbb8331d2a
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-07-09 06:31:48 +0300

    finish task 5 free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 
-
 void free_dlistint(dlistint_t *head)
 {  
-    dlistint_t **temp = &head;
-    (*temp)->prev = *temp;
-    *temp = (*temp)->next;
-    while(*temp)
+    dlistint_t *temp = head;
+    while (temp != NULL)
     {
-        free((*temp)->prev);
+        temp = temp->next;
+        free(temp->prev);
+        if (temp != NULL)
+            temp->prev->next = NULL;
     }
-    free(*temp);
 }
