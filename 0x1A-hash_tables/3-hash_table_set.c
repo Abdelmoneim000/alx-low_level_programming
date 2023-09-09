e0e101ed7263c24f0a094d1729430ed872ec3832
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-09-09 19:19:52 +0300

    hello world

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,14 +8,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         return (0);
 
     index = key_index((const unsigned char *)key, ht->size);
-    if (ht->array[index] == NULL)
-    {
-        node = malloc(sizeof(hash_node_t));
-        if (node == NULL)
+      node = malloc(sizeof(hash_node_t));
+        if(node == NULL)
         return 0;
         node->key = strdup(key);
         node->value = strdup(value);
         node->next = NULL;
+    if (ht->array[index] == NULL)
+    {
+    ht->array[index] = node;
     }
     else
     {
@@ -27,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                     return 0;
             return 1; 
         }
-        node = malloc(sizeof(hash_node_t));
+       
         node->next = ht->array[index];
         ht->array[index] = node;
     return 1;
