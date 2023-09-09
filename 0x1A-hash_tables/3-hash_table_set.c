aee0c90d6c48c913921dbb75a6968220e31b1acb
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-09-09 18:09:50 +0300

    try

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,46 +1,34 @@
 #include "hash_tables.h"
-/**
- * hash_table_set - Set the value of a key in the hash table
- * @ht: The hash table
- * @key: The key to set
- * @value: The value to set the key to
- *
- * Return: On success, returns 1. On failure, returns 0.
- */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current_node;
+	unsigned long int index;
+	hash_node_t *array, *new_node;
 
-    if (ht == NULL || key == NULL || value == NULL)
-    {
-        return 0;
-    }
+	index = key_index((const unsigned char *)key, ht->size);
 
-    index = hash_djb2((const unsigned char *)key) % ht->size;
+	array = ht->array[index];
 
-    current_node = ht->array[index];
-    while (current_node != NULL)
-    {
-        if (strcmp(current_node->key, key) == 0)
-        {
-            free(current_node->value);
-            current_node->value = strdup(value);
-            return 1;
-        }
-        current_node = current_node->next;
-    }
+	while(array)
+	{
+		if(strcmp(key, array->key) == 0)
+		{
+			free(array->value);
+			array->value = strdup(value);
+			if(array->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		array = array->next;
+	}
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        return 0;
-    }
+	new_node = malloc(sizeof(hash_node_t *));
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return 1;
+	return (1);
 }
