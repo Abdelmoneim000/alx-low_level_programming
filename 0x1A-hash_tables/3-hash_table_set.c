65ff1f06ce6581cafabc28e9836252de2c9b8e12
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-09-09 18:11:03 +0300

    try

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,6 +5,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *array, *new_node;
 
+    if(key == NULL || value == NULL)
+    {
+        return (0);
+    }
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	array = ht->array[index];
