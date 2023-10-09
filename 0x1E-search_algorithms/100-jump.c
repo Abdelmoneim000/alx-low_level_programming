b9f1aaf7797352a72915cd46eb2add0b3f776490
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-10 01:59:48 +0300

    changing the code logic to be the same as jumping algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,35 +1,37 @@
 #include "search_algos.h"
 
-
+/**
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 
 int jump_search(int *array, size_t size, int value)
 {
-	int i;
-	int j;
-	i = 0;
+	int jumpSize = 0;
+	int left, right = 0;
+	int i = 0;
 
-	if(array == NULL || size == 0)
+	jumpSize = (int)sqrt(size);
+	while (right < (int)size && array[right] < value)
 	{
-		return (-1);
+		printf("Value checked array[%d] = [%d]\n", right, array[right]);
+		left = right;
+		right += jumpSize;
 	}
 
-	while (array[i] < value && i < (int)size)
+	printf("Value found between indexes [%d] and [%d]\n", left, right);
+	for (i = left; i <= right && i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		j = i;
-		i = i + 3;
-	}
-
-	printf("Value found between indexes [%d] and [%d]\n", j, i);
-	while (j <= i)
-	{
-		printf("Value checked array[%d] = [%d]\n", j, array[j]);
-
-		if (array[j] == value)
+		if (array[i] == value)
 		{
-			return (j);
+			return (i);
 		}
-		j++;
 	}
 
 	return (-1);
