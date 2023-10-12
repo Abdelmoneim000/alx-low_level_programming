e5e30b68cea9cadfe7f10dd03701e1eb42a14846
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-12 03:17:33 +0300

    finish task 103 exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,8 +11,10 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 /*Helper functions*/
 void print_arr(int *array, int *left, int *right);
+int _binary_search(int *array, size_t left, size_t right, int value);
 
 #endif
